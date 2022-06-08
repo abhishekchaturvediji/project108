5359d75959988e3faa9d890633b983c8cce76cbf
abhishekchaturvediji <abhichaube321@gmail.com>
2022-06-08 23:28:53 +0530

    hoisting and closure

---
+++ index.js
@@ -0,0 +1,16 @@
+// hoisting
+heightCalculator('Tinu', 125);
+
+var a  = 'Andhi';
+// console.log(" :: a :: ", a , window.heightCalculator);
+
+let b = 125;
+
+function heightCalculator(a , b){
+    var c = 'hi' ;
+    console.log(` ${a} your height is ${b}`)
+}
+
+// what is window oject ?
+// closure accessible scopes ?
+
